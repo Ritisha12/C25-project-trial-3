6ecd0cdda70880bbdc8c06a05ebd26101bd906a5
Ritisha12 <68296139+Ritisha12@users.noreply.github.com>
2020-08-14 13:16:14 -0400

    Update paper.js

--- paper.js
+++ paper.js
@@ -7,7 +7,7 @@ var options={
     density:1.2
 }
 this.body=Bodies.circle(x,y,30,options)
-this.image=loadImage("images/paper.png")
+this.image=loadImage("paper.png")
 this.radius=30;
 World.add(world,this.body)
 }
